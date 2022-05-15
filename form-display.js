64fb4f590dda098d44d0af215f2bd3db31eae3f7
Kanishk K <42405147+Kanishk99@users.noreply.github.com>
2022-05-16 04:44:05 +0530

    Added login modal

---
+++ form-display.js
@@ -0,0 +1,10 @@
+
+function open_form(){
+    console.log('test');
+    document.querySelector('.bg-modal').style.display = 'flex'; 
+    
+}
+function close_form(){
+    document.querySelector('.bg-modal').style.display = 'none';
+}
+
